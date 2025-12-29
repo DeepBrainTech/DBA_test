8488ec18314aa1b227baddfd2f29e1d4fa6423fb
wjm <eroih1@163.com>
2025-12-29 15:31:15 -0500

    fix

--- components/sections/HonorWall.tsx
+++ components/sections/HonorWall.tsx
@@ -75,3 +75,4 @@ export default function HonorWall({ achievements }: HonorWallProps) {
   );
 }
 
+
--- components/sections/TrustBadges.tsx
+++ components/sections/TrustBadges.tsx
@@ -36,3 +36,4 @@ export default function TrustBadges({ badges }: TrustBadgesProps) {
   );
 }
 
+
